c346c7a981282e1db4b09487cbefd6a5059cd4e4
AlexandrAnatoliev <per-1986@list.ru>
2026-05-04 19:12:47 +0300

    fix: add index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>send-invoice</title>
+  <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    test text
+</body>
+</html>
