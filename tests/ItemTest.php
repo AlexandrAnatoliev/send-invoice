2f146057ec572f3bc7723972d4ced57095e3f995
AlexandrAnatoliev <per-1986@list.ru>
2026-05-06 14:22:38 +0300

    fix: add classes autoload

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
-require_once 'src/Item.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 use sendInvoice\Item;
 
