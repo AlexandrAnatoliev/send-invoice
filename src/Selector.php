df42e076ed70e25f96dd2657817fa00eda24eece
AlexandrAnatoliev <per-1986@list.ru>
2026-05-08 18:28:44 +0300

    fix: add Selector class

---
+++ src/Selector.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace sendInvoice;
+
+/**
+ * Selector represents a quantity-block for the order calculator.
+ *
+ * @package sendInvoice
+ */
+class Selector extends FormElement
+{
+    protected const CSS_FILE = '../styles/Selector.css';
+
+    public function render(): string
+    {
+        return '';
+    }
+}
