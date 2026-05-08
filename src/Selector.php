c2063b28bdcdc0afbef64948f5554ce8980b7ae1
AlexandrAnatoliev <per-1986@list.ru>
2026-05-08 19:06:19 +0300

    fix(Selector): update constructor

--- src/Selector.php
+++ src/Selector.php
@@ -10,6 +10,29 @@ namespace sendInvoice;
 class Selector extends FormElement
 {
     protected const CSS_FILE = '../styles/Selector.css';
+    protected int $min;
+    protected int $max;
+    protected int $step;
+
+    /**
+     * Create a new instance.
+     *
+     * @param $name Selector name
+     * @param $min  Selector min value
+     * @param $max  Selector max value
+     * @param $step Selector oscillation step
+     */
+    public function __construct(
+        string $name,
+        int $min,
+        int $max,
+        int $step,
+    ) {
+        parent::__construct($name);
+        $this->min = $min;
+        $this->min = $max;
+        $this->min = $step;
+    }
 
     public function render(): string
     {
