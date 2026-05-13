8b32373db723bb40031bebb094a3ff7d00149781
AlexandrAnatoliev <per-1986@list.ru>
2026-05-13 19:22:02 +0300

    docs: update Configs docs

--- src/Config.php
+++ src/Config.php
@@ -7,9 +7,11 @@ class Config
     private array $env;
 
     /**
+     * При создании инициализирует массив с настройками
+     * либо из переменной окружения $_ENV
+     * либо из тестового массива
      *
-     *
-     * @param  $env
+     * @param  $env - Тестовый массив
      */
     public function __construct(?array $env = null)
     {
@@ -23,6 +25,13 @@ class Config
         }
     }
 
+    /**
+     * Возвращает значение настроек конфига по ключу
+     *
+     * @param  $key     - Ключ
+     * @param  $default - Дефолтное значение (если ключ не найден)
+     * @return Значение настроек конфига
+     */
     public function get($key, $default = 'Заполните настройки'): string
     {
         return $this->env[$key] ?? getenv($key) ?: $default;
